f5cb3de501aa5be8a700e5386a0cd93e20445c93
Sridhar Ratnakumar <srid@srid.ca>
2018-07-18 11:40:16 -0400

    Remove Rhyolite.Backend.TH

--- backend/Rhyolite/Backend/Schema/TH.hs
+++ backend/Rhyolite/Backend/Schema/TH.hs
@@ -16,8 +16,12 @@
 module Rhyolite.Backend.Schema.TH where
 
 import Control.Lens ((%~), _head)
+import Control.Monad
+import Control.Monad.State (mapStateT)
 import Data.Char (toLower)
 import Data.Int (Int64)
+import Data.Monoid
+import Database.Groundhog
 import Database.Groundhog.Core
 import Data.Semigroup ((<>))
 import Data.List (isPrefixOf)
@@ -25,7 +29,43 @@ import Database.Groundhog.TH (migrationFunction, namingStyle, mkDbFieldName, mkE
 import Database.Groundhog.TH.Settings (PersistDefinitions(..))
 import Language.Haskell.TH
 
+import Rhyolite.TH (conName)
 import Rhyolite.Backend.Schema.Class
+
+deriveNewtypePersistBackend :: (TypeQ -> TypeQ) -> (TypeQ -> TypeQ) -> Name -> Name -> DecsQ
+deriveNewtypePersistBackend toT fromT to from =
+  liftM (:[]) $ liftM3 (InstanceD Nothing) (cxt [appT (conT ''PersistBackend) (fromT m), appT (conT ''Monad) m]) (appT (conT ''PersistBackend) (toT m)) $ liftM2 (<>) typeInstances functions
+  where
+    m = varT $ mkName "m"
+    typeInstances = do
+      phantomDbInst <- tySynInstD ''PhantomDb $ tySynEqn [toT m] $ appT (conT ''PhantomDb) (fromT m)
+      tableAnalysisInst <- tySynInstD ''TableAnalysis $ tySynEqn [toT m] $ appT (conT ''TableAnalysis) (fromT m)
+      return [phantomDbInst, tableAnalysisInst]
+    n =: e = valD (varP n) (normalB e) []
+    functions = sequence $
+      [ 'insert =: [| $(conE to) . insert |]
+      , 'insert_ =: [| $(conE to) . insert_ |]
+      , 'insertBy =: [| \u v -> $(conE to) $ insertBy u v |]
+      , 'insertByAll =: [| $(conE to) . insertByAll |]
+      , 'replace =: [| \k v -> $(conE to) $ replace k v |]
+      , 'replaceBy =: [| \u v -> $(conE to) $ replaceBy u v |]
+      , 'select =: [| $(conE to) . select |]
+      , 'selectAll =: [| $(conE to) selectAll |]
+      , 'get =: [| $(conE to) . get |]
+      , 'getBy =: [| $(conE to) . getBy |]
+      , 'update =: [| \us c -> $(conE to) $ update us c |]
+      , 'delete =: [| $(conE to) . delete |]
+      , 'deleteBy =: [| $(conE to) . deleteBy |]
+      , 'deleteAll =: [| $(conE to) . deleteAll |]
+      , 'count =: [| $(conE to) . count |]
+      , 'countAll =: [| $(conE to) . countAll |]
+      , 'project =: [| \p o -> $(conE to) $ project p o |]
+      , 'migrate =: [| \i v -> mapStateT $(conE to) $ migrate i v |]
+      , 'executeRaw =: [| \c q p -> $(conE to) $ executeRaw c q p |]
+      , 'queryRaw =: [| \c q p f -> $(conE to) $ queryRaw c q p $ \rp -> $(varE from) $ f $ $(conE to) rp |]
+      , 'insertList =: [| $(conE to) . insertList |]
+      , 'getList =: [| $(conE to) . getList |]
+      ]
 
 makeDefaultKeyIdInt64 :: Name -> Name -> Q [Dec]
 makeDefaultKeyIdInt64 n k = do
