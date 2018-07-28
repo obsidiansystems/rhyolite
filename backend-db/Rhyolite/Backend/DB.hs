f17eff8ed5e15c0606bdccdab080f0be9be18543
Ali Abrar <aliabrar@gmail.com>
2018-07-28 14:03:57 -0400

    Separate database and schema packages from rhyolite-backend

---
+++ backend-db/Rhyolite/Backend/DB.hs
@@ -0,0 +1,163 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Rhyolite.Backend.DB where
+
+import Control.Arrow (first)
+import Control.Monad (liftM, void)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Trans.Control (MonadBaseControl)
+import Data.ByteString (ByteString)
+import Data.Functor.Identity (Identity (..))
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (listToMaybe)
+import Data.Pool (Pool, createPool, withResource)
+import Data.String (fromString)
+import Data.Time (UTCTime)
+import Database.Groundhog.Core
+import Database.Groundhog.Expression (Expression, ExpressionOf, Unifiable)
+import Database.Groundhog.Generic (mapAllRows)
+import Database.Groundhog.Generic.Sql (operator)
+import Database.Groundhog.Postgresql (Postgresql (..), SqlDb, isFieldNothing, runDbConn)
+import Database.PostgreSQL.Simple (close, connectPostgreSQL)
+
+import Data.Map.Monoidal (MonoidalMap, pattern MonoidalMap)
+import Rhyolite.Backend.DB.PsqlSimple
+import Rhyolite.Backend.Schema
+import Rhyolite.Backend.Schema.Class
+import Rhyolite.Schema
+
+
+-- | Convenience function for getting the first result of a projection as a 'Maybe'
+project1
+  :: ( PersistEntity v, EntityConstr v c
+     , Projection' p conn (RestrictionHolder v c) a
+     , HasSelectOptions opts conn (RestrictionHolder v c)
+     , HasLimit opts ~ HFalse
+     , PersistBackend m, ProjectionDb p conn, PhantomDb m ~ conn )
+  => p -> opts -> m (Maybe a)
+project1 p opts = fmap listToMaybe $ project p $ opts `limitTo` 1
+
+-- | Convenience function for getting the first result of a projection. Calls
+-- 'error' when there is no result.
+project1'
+  :: ( PersistEntity v, EntityConstr v c
+     , Projection' p conn (RestrictionHolder v c) a
+     , HasSelectOptions opts conn (RestrictionHolder v c)
+     , HasLimit opts ~ HFalse
+     , PersistBackend m, ProjectionDb p conn, PhantomDb m ~ conn )
+  => p -> opts -> m a
+project1' p opts = project1 p opts >>= \case
+  Nothing -> error "project1' expected a result, but got none"
+  Just a -> pure a
+
+-- | Will return all matching instances of the given constructor
+selectMap :: forall a (m :: * -> *) v (c :: (* -> *) -> *) t.
+             (ProjectionDb t (PhantomDb m),
+              ProjectionRestriction t (RestrictionHolder v c), DefaultKeyId v,
+              Projection t v,
+              EntityConstr v c,
+              HasSelectOptions a (PhantomDb m) (RestrictionHolder v c),
+              PersistBackend m, Ord (IdData v),
+              AutoKey v ~ DefaultKey v) =>
+             t -> a -> m (Map (Id v) v)
+--selectMap :: (PersistBackend m, PersistEntity v, EntityConstr v c, Constructor c, Projection (c (ConstructorMarker v)) (PhantomDb m) (RestrictionHolder v c) v, HasSelectOptions opts (PhantomDb m) (RestrictionHolder v c), AutoKey v ~ DefaultKey v, DefaultKeyId v, Ord (IdData v)) => c (ConstructorMarker v) -> opts -> m (Map (Id v) v)
+selectMap constr = liftM (Map.fromList . map (first toId)) . project (AutoKeyField, constr)
+
+selectMap' :: forall a (m :: * -> *) v (c :: (* -> *) -> *) t.
+              (ProjectionDb t (PhantomDb m),
+              ProjectionRestriction t (RestrictionHolder v c), DefaultKeyId v,
+              Projection t v,
+              EntityConstr v c,
+              HasSelectOptions a (PhantomDb m) (RestrictionHolder v c),
+              PersistBackend m, Ord (IdData v),
+              AutoKey v ~ DefaultKey v) =>
+              t -> a -> m (MonoidalMap (Id v) v)
+selectMap' constr = fmap MonoidalMap . selectMap constr
+
+fieldIsJust, fieldIsNothing :: (NeverNull a, Expression db r f, PrimitivePersistField a, Projection f (Maybe a), Unifiable f (Maybe a)) => f -> Cond db r
+
+--fieldIsNothing :: forall db r a b x. (r ~ RestrictionHolder a x, EntityConstr a x, Expression db r (Maybe b), Unifiable (Field a x (Maybe b)) (Maybe b), NeverNull b, PrimitivePersistField b) => Field a x (Maybe b) -> Cond db r
+fieldIsNothing = isFieldNothing
+
+--fieldIsJust :: forall db r a b x. (r ~ RestrictionHolder a x, EntityConstr a x, Expression db r (Maybe b), Unifiable (Field a x (Maybe b)) (Maybe b), NeverNull b, PrimitivePersistField b) => Field a x (Maybe b) -> Cond db r
+--fieldIsJust f = f /=. (Nothing :: Maybe b)
+fieldIsJust f = Not $ isFieldNothing f
+
+getTime :: PersistBackend m => m UTCTime
+getTime = do
+  Just [PersistUTCTime t] <- queryRaw False "select current_timestamp(3) at time zone 'utc'" [] id
+  return t
+
+withTime :: PersistBackend m => (UTCTime -> m a) -> m a
+withTime a = do
+  now <- getTime
+  a now
+
+openDb :: ByteString -> IO (Pool Postgresql)
+openDb dbUri = do
+  let openPostgresql = liftM Postgresql $ connectPostgreSQL dbUri
+      closePostgresql (Postgresql p) = close p
+  createPool openPostgresql closePostgresql 1 5 20
+
+-- | Runs a database action using a given pool of database connections
+-- The 'f' parameter can be used to represent additional information about
+-- how/where to run the database action (e.g., in a particular schema).
+-- See instances below.
+class RunDb f where
+  runDb :: ( MonadIO m
+           , MonadBaseControl IO m
+           , ConnectionManager cm conn
+           , PostgresRaw (DbPersist conn m)
+           , PersistBackend (DbPersist conn m))
+        => f (Pool cm)
+        -> DbPersist conn m b
+        -> m b
+
+-- | Runs a database action in the public schema
+instance RunDb Identity where
+  runDb (Identity db) = withResource db . runDbConn . withSchema (SchemaName "public")
+
+-- | Runs a database action in a specific schema
+instance RunDb WithSchema where
+  runDb (WithSchema schema db) = withResource db . runDbConn . withSchema schema
+
+getSearchPath :: PersistBackend m => m String
+getSearchPath = do
+  [searchPath] :: [String] <- queryRaw False "SHOW search_path" [] $ mapAllRows (fmap fst . fromPersistValues)
+  return searchPath
+
+setSearchPath :: (Monad m, PostgresRaw m) => String -> m ()
+setSearchPath sp = void $ execute_ $ "SET search_path TO " `mappend` fromString sp
+
+setSchema :: (Monad m, PostgresRaw m) => SchemaName -> m ()
+setSchema schema = void $ execute [sql| SET search_path TO ?,"$user",public |] (Only schema)
+
+-- | Sets the search path to a particular schema, runs an action in that schema, and resets the search path
+withSchema :: (PostgresRaw m, PersistBackend m) => SchemaName -> m r -> m r
+withSchema schema a = do
+  sp <- getSearchPath
+  setSchema schema
+  r <- a
+  setSearchPath sp
+  return r
+
+ensureSchemaExists :: (Monad m, PostgresRaw m)
+                   => SchemaName
+                   -> m ()
+ensureSchemaExists schema = void $ execute [sql| CREATE SCHEMA IF NOT EXISTS ? |] (Only schema)
+
+ilike :: (SqlDb db, ExpressionOf db r a a') => a -> String -> Cond db r
+ilike a b = CondRaw $ operator 40 " ILIKE " a b
---
+++ backend-db/Rhyolite/Backend/Schema/TH.hs
@@ -0,0 +1,120 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans -fno-warn-redundant-constraints #-}
+
+module Rhyolite.Backend.Schema.TH where
+
+import Control.Lens ((%~), _head)
+import Control.Monad
+import Control.Monad.State (mapStateT)
+import Data.Char (toLower)
+import Data.Int (Int64)
+import Database.Groundhog
+import Database.Groundhog.Core
+import Data.Semigroup ((<>))
+import Data.List (isPrefixOf)
+import Database.Groundhog.TH (migrationFunction, namingStyle, mkDbFieldName, mkExprFieldName, mkExprSelectorName, mkPersist, defaultCodegenConfig)
+import Database.Groundhog.TH.Settings (PersistDefinitions(..))
+import Language.Haskell.TH
+
+import Rhyolite.TH (conName)
+import Rhyolite.Backend.Schema.Class
+
+deriveNewtypePersistBackend :: (TypeQ -> TypeQ) -> (TypeQ -> TypeQ) -> Name -> Name -> DecsQ
+deriveNewtypePersistBackend toT fromT to from =
+  liftM (:[]) $ liftM3 (InstanceD Nothing) (cxt [appT (conT ''PersistBackend) (fromT m), appT (conT ''Monad) m]) (appT (conT ''PersistBackend) (toT m)) $ liftM2 (<>) typeInstances functions
+  where
+    m = varT $ mkName "m"
+    typeInstances = do
+      phantomDbInst <- tySynInstD ''PhantomDb $ tySynEqn [toT m] $ appT (conT ''PhantomDb) (fromT m)
+      tableAnalysisInst <- tySynInstD ''TableAnalysis $ tySynEqn [toT m] $ appT (conT ''TableAnalysis) (fromT m)
+      return [phantomDbInst, tableAnalysisInst]
+    n =: e = valD (varP n) (normalB e) []
+    functions = sequence $
+      [ 'insert =: [| $(conE to) . insert |]
+      , 'insert_ =: [| $(conE to) . insert_ |]
+      , 'insertBy =: [| \u v -> $(conE to) $ insertBy u v |]
+      , 'insertByAll =: [| $(conE to) . insertByAll |]
+      , 'replace =: [| \k v -> $(conE to) $ replace k v |]
+      , 'replaceBy =: [| \u v -> $(conE to) $ replaceBy u v |]
+      , 'select =: [| $(conE to) . select |]
+      , 'selectAll =: [| $(conE to) selectAll |]
+      , 'get =: [| $(conE to) . get |]
+      , 'getBy =: [| $(conE to) . getBy |]
+      , 'update =: [| \us c -> $(conE to) $ update us c |]
+      , 'delete =: [| $(conE to) . delete |]
+      , 'deleteBy =: [| $(conE to) . deleteBy |]
+      , 'deleteAll =: [| $(conE to) . deleteAll |]
+      , 'count =: [| $(conE to) . count |]
+      , 'countAll =: [| $(conE to) . countAll |]
+      , 'project =: [| \p o -> $(conE to) $ project p o |]
+      , 'migrate =: [| \i v -> mapStateT $(conE to) $ migrate i v |]
+      , 'executeRaw =: [| \c q p -> $(conE to) $ executeRaw c q p |]
+      , 'queryRaw =: [| \c q p f -> $(conE to) $ queryRaw c q p $ \rp -> $(varE from) $ f $ $(conE to) rp |]
+      , 'insertList =: [| $(conE to) . insertList |]
+      , 'getList =: [| $(conE to) . getList |]
+      ]
+
+makeDefaultKeyIdInt64 :: Name -> Name -> Q [Dec]
+makeDefaultKeyIdInt64 n k = do
+  pv <- newName "pv"
+  [d|
+    instance IdDataIs $(conT n) Int64 => DefaultKeyId $(conT n) where
+      toIdData _ dk = case $(lamE [conP k [varP pv]] (varE pv)) dk of
+        PersistInt64 x -> x
+        _ -> error "makeDefaultKeyIdInt64: pattern match failure (this should be impossible)"
+      fromIdData _ = $(conE k) . PersistInt64
+    |]
+
+makeDefaultKeyIdSimple :: Name -> Name -> Q [Dec]
+makeDefaultKeyIdSimple n k = do
+  pv <- newName "pv"
+  [d|
+    instance DefaultKeyId $(conT n) where
+      toIdData _ = $(lamE [conP k [varP pv]] (varE pv))
+      fromIdData _ = $(conE k)
+    |]
+
+
+-- | Run Database.Groundhog.TH.mkPersist with rhyolite-specific defaults
+--
+-- Record field names are expected to look like _dataTypeName_fieldName
+mkRhyolitePersist :: Maybe String -> PersistDefinitions -> Q [Dec]
+mkRhyolitePersist migrationFunctionName = mkPersist $ defaultCodegenConfig
+  { migrationFunction = migrationFunctionName
+  , namingStyle =
+    let ns = namingStyle defaultCodegenConfig
+        dropPrefix p x = if p `isPrefixOf` x then drop (length p) x else error $ "mkRhyolitePersist: dropPrefix: expected string with prefix " <> show p <> ", got string " <> show x
+    in ns { mkDbFieldName = \typeName cN conPos fieldName fieldPos ->
+             mkDbFieldName ns typeName cN conPos (dropPrefix ("_" <> (_head %~ toLower) typeName <> "_") fieldName) fieldPos
+          , mkExprFieldName = \typeName cN conPos fieldName fieldPos ->
+             mkExprFieldName ns typeName cN conPos (dropPrefix "_" fieldName) fieldPos
+          , mkExprSelectorName  = \typeName cN fieldName fieldPos ->
+             mkExprSelectorName ns typeName cN (dropPrefix "_" fieldName) fieldPos
+          }
+  }
+
+makePersistFieldNewtype :: Name -> Q [Dec]
+makePersistFieldNewtype t = do
+  TyConI (NewtypeD _ _ _ _ con _) <- reify t
+  let c = conName con
+  xName <- newName "x"
+  [d| instance PersistField $(conT t) where
+        persistName _ = $(stringE $ nameBase t)
+        toPersistValues $(conP c [varP xName]) = toPersistValues $(varE xName)
+        fromPersistValues pv = do
+          (x, pv') <- fromPersistValues pv
+          return ($(conE c) x, pv')
+        dbType p $(conP c [varP xName]) = dbType p $(varE xName)
+    |]
