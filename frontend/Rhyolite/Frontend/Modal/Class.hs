56e60f48e050f0060cf645a2aa83a5529c91f8f8
Elliot Cameron <eacameron@gmail.com>
2019-07-29 17:40:30 -0400

    rhyolite-frontend: Improve docs of Rhyolite.Modal.Class

--- frontend/Rhyolite/Frontend/Modal/Class.hs
+++ frontend/Rhyolite/Frontend/Modal/Class.hs
@@ -13,7 +13,16 @@ import Obelisk.Route.Frontend (RoutedT, askRoute, runRoutedT)
 -- | The class of monads supporting a 'tellModal' operation which will open a modal
 --   that stays on top of all other content.
 class HasModal t m where
+  -- If 'm' is the monad that supports 'tellModal' then 'ModalM m' is the monad that the modal itself is in,
+  -- which, notably, probably doesn't support 'tellModal'.
   type ModalM m :: * -> *
+
+  -- | Opens a modal when the given event fires. The event carries a function which:
+  --     * takes a "close" event triggered when the user signifies that they want to close the modal,
+  --     * builds content in 'ModalM m',
+  --     * returns a "close" event which will be used to actually close the modal.
+  --
+  --   For example, a modal may choose not to be closable by simply ignoring it's input and returning 'never'.
   tellModal :: Event t (Event t () -> ModalM m (Event t ())) -> m ()
 
   default tellModal :: (MonadTrans f, m ~ f m', HasModal t m', Monad m', ModalM (f m') ~ ModalM m') => Event t (Event t () -> ModalM m (Event t ())) -> m ()
