54a0096f9f53ad47b642e258fe1c0f919af3ebf6
Elliot Cameron <eacameron@gmail.com>
2019-09-16 23:03:28 -0400

    test: Add rhyolite-logging to test suite

--- test/src-bin/test.hs
+++ test/src-bin/test.hs
@@ -8,6 +8,7 @@ import Rhyolite.Backend.DB -- backend-db
 import Rhyolite.Backend.Snap.Util.FileServe.Stream -- backend-snap
 import Rhyolite.Frontend.App -- frontend
 import Rhyolite.SemiMap -- datastructures
+import Rhyolite.Logging -- logging
 
 main :: IO ()
 main = putStrLn "rhyolite submodules were successfully built"
