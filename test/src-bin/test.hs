7fadd288d1e426cca85d4e78928d40c26d76c204
Elliot Cameron <eacameron@gmail.com>
2019-09-16 23:01:30 -0400

    test: Order imports

--- test/src-bin/test.hs
+++ test/src-bin/test.hs
@@ -1,10 +1,10 @@
 import Rhyolite.Aeson.Orphans -- aeson-orphans
+import Rhyolite.App --common
+import Rhyolite.Backend.App -- backend
 import Rhyolite.Backend.DB -- backend-db
 import Rhyolite.Backend.Snap.Util.FileServe.Stream -- backend-snap
-import Rhyolite.Backend.App -- backend
-import Rhyolite.App --common
-import Rhyolite.SemiMap -- datastructures
 import Rhyolite.Frontend.App -- frontend
+import Rhyolite.SemiMap -- datastructures
 
 main :: IO ()
 main = putStrLn "rhyolite submodules were successfully built"
