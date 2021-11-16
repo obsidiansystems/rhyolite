b8110b34a5b599e020c024b29c7a6c0f1f27ceff
Anurag Ohri <anuragohri92@gmail.com>
2021-11-16 15:31:57 +0530

    Removed the package backend-snap

--- test/src-bin/test.hs
+++ test/src-bin/test.hs
@@ -5,7 +5,6 @@ import Rhyolite.Aeson.Orphans -- aeson-orphans
 import Rhyolite.App --common
 import Rhyolite.Backend.App -- backend
 import Rhyolite.Backend.DB -- backend-db
-import Rhyolite.Backend.Snap.Util.FileServe.Stream -- backend-snap
 import Rhyolite.Frontend.App -- frontend
 import Rhyolite.SemiMap -- datastructures
 
