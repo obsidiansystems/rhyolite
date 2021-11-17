613c91a9af09d08ee59da5c91a9f40b3079b3e64
Ali Abrar <aliabrar@gmail.com>
2021-11-16 21:51:37 -0500

    Add postgresql-simple-class

---
+++ postgresql-simple-class/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for postgresql-simple-class
+
+## 0.1.0.0
+
+* Create class PsqlConn with various instances
---
+++ postgresql-simple-class/src/Database/PostgreSQL/Simple/Class.hs
@@ -0,0 +1,93 @@
+{-# Language DefaultSignatures #-}
+{-# Language GADTs #-}
+module Database.PostgreSQL.Simple.Class where
+
+import Control.Monad (void)
+import Control.Monad.Reader (ReaderT)
+import Control.Monad.State as State
+import qualified Control.Monad.State.Strict as Strict
+import Control.Monad.Trans (MonadIO, MonadTrans, lift, liftIO)
+import qualified Control.Monad.Trans.Cont
+import qualified Control.Monad.Trans.Except
+import qualified Control.Monad.Trans.Identity
+import Control.Monad.Trans.Maybe (MaybeT)
+import qualified Control.Monad.Trans.RWS.Lazy
+import qualified Control.Monad.Trans.RWS.Strict
+import qualified Control.Monad.Trans.Writer.Lazy
+import qualified Control.Monad.Trans.Writer.Strict
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
+import Data.Int (Int64)
+import Database.PostgreSQL.Simple (Connection)
+import Database.PostgreSQL.Simple.FromRow (FromRow, RowParser)
+import Database.PostgreSQL.Simple.ToRow (ToRow)
+import Database.PostgreSQL.Simple.Types (Query)
+
+class PsqlConn m where
+  askConn :: m Connection
+  default askConn :: ( m ~ t n, MonadTrans t, Monad n, PsqlConn n) => m Connection
+  askConn = lift askConn
+  execute :: ToRow q => Query -> q -> m Int64
+  default execute :: ( m ~ t n , MonadTrans t , Monad n , PsqlConn n , ToRow q) => Query -> q -> m Int64
+  execute psql qs = lift $ execute psql qs
+
+  execute_ :: Query -> m Int64
+  default execute_ :: (m ~ t n, PsqlConn n, Monad n, MonadTrans t) =>  Query -> m Int64
+  execute_ = lift . execute_
+
+  executeMany :: ToRow q => Query -> [q] -> m Int64
+  default executeMany :: (m ~ t n, ToRow q, PsqlConn n, Monad n, MonadTrans t) => Query -> [q] -> m Int64
+  executeMany psql qs = lift $ executeMany psql qs
+
+  query :: (ToRow q, FromRow r) => Query -> q -> m [r]
+  default query :: (m ~ t n, ToRow q, FromRow r, PsqlConn n, Monad n, MonadTrans t) => Query -> q -> m [r]
+  query psql qs = lift $ query psql qs
+
+  query_ :: FromRow r => Query -> m [r]
+  default query_ :: (m ~ t n, FromRow r, PsqlConn n, Monad n, MonadTrans t) => Query -> m [r]
+  query_ = lift . query_
+
+  queryWith :: ToRow q => RowParser r -> Query -> q -> m [r]
+  default queryWith :: (m ~ t n, ToRow q, PsqlConn n, Monad n, MonadTrans t) => RowParser r -> Query -> q -> m [r]
+  queryWith parser psql qs = lift $ queryWith parser psql qs
+
+  queryWith_ :: RowParser r -> Query -> m [r]
+  default queryWith_ :: (m ~ t n, PsqlConn n, Monad n, MonadTrans t) => RowParser r -> Query -> m [r]
+  queryWith_ parser psql = lift $ queryWith_ parser psql
+
+  formatQuery :: ToRow q => Query -> q -> m BS.ByteString
+  default formatQuery :: (m ~ t n, ToRow q, PsqlConn n, Monad n, MonadTrans t) => Query -> q -> m BS.ByteString
+  formatQuery psql qs = lift $ formatQuery psql qs
+
+  returning :: (ToRow q, FromRow r) => Query -> [q] -> m [r]
+  default returning :: (m ~ t n, ToRow q, FromRow r, PsqlConn n, Monad n, MonadTrans t) => Query -> [q] -> m [r]
+  returning psql qs = lift $ returning psql qs
+
+
+traceQuery :: (PsqlConn m, MonadIO m, ToRow q, FromRow r) => Query -> q -> m [r]
+traceQuery p q = do
+  s <- formatQuery p q
+  liftIO (BSC.putStrLn s)
+  query p q
+
+traceExecute :: (PsqlConn m, MonadIO m, ToRow q) => Query -> q -> m Int64
+traceExecute p q = do
+  s <- formatQuery p q
+  liftIO (BSC.putStrLn s)
+  execute p q
+
+traceExecute_ :: (PsqlConn m, MonadIO m, ToRow q) => Query -> q -> m ()
+traceExecute_ p q = void $ traceExecute p q
+
+instance (Monad m, PsqlConn m) => PsqlConn (StateT s m)
+instance (Monad m, PsqlConn m) => PsqlConn (Strict.StateT s m)
+instance (Monad m, PsqlConn m) => PsqlConn (MaybeT m)
+instance (Monad m, PsqlConn m) => PsqlConn (ReaderT r m)
+
+instance (Monad m, PsqlConn m) => PsqlConn (Control.Monad.Trans.Except.ExceptT e m)
+instance (Monad m, PsqlConn m) => PsqlConn (Control.Monad.Trans.Identity.IdentityT m)
+instance (Monoid w, Monad m, PsqlConn m) => PsqlConn (Control.Monad.Trans.Writer.Strict.WriterT w m)
+instance (Monoid w, Monad m, PsqlConn m) => PsqlConn (Control.Monad.Trans.Writer.Lazy.WriterT w m)
+instance (Monad m, PsqlConn m) => PsqlConn (Control.Monad.Trans.Cont.ContT r m)
+instance (Monoid w, Monad m, PsqlConn m) => PsqlConn (Control.Monad.Trans.RWS.Strict.RWST r w s m)
+instance (Monoid w, Monad m, PsqlConn m) => PsqlConn (Control.Monad.Trans.RWS.Lazy.RWST r w s m)
