28a48adf07e30cf0d74648a575ac79da81c2be62
Ali Abrar <ali.abrar@obsidian.systems>
2021-11-22 23:57:11 -0500

    docs: Update Backend.WebSocket

--- backend/Rhyolite/Backend/WebSocket.hs
+++ backend/Rhyolite/Backend/WebSocket.hs
@@ -27,6 +27,7 @@ withWebsocketsConnection f = runWebSocketsSnap $ withPendingWebsocketConnection 
   where
     logger str e = putStrLn $ "withWebsocketsConnection: " <> (if null str then "" else str <> ": ") <> show e
 
+-- | Handle a websocket connection that is in a "pending" state (i.e., that hasn't yet been accepted)
 withPendingWebsocketConnection :: (String -> SomeException -> IO ()) -> (WS.Connection -> IO ()) -> WS.PendingConnection -> IO ()
 withPendingWebsocketConnection logger f pc = do
   conn <- WS.acceptRequest pc
