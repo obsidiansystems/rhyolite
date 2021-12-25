64f5ff33f44c23c6f362927f12b0c39d08d2f7e4
Ali Abrar <ali.abrar@obsidian.systems>
2021-12-24 23:08:24 -0500

    account: add functions for creating accounts and setting passwords

--- account/backend/src/Rhyolite/Backend/Account.hs
+++ account/backend/src/Rhyolite/Backend/Account.hs
@@ -8,17 +8,23 @@ Description:
 module Rhyolite.Backend.Account
   ( createAccount
   , login
+  , ensureAccountExists
+  , setAccountPasswordHash
+  , makePasswordHash
+  , resetPassword
   ) where
 
 import Control.Monad (guard)
 import Control.Monad.Trans.Maybe
 import Crypto.PasswordStore
 import Data.Aeson
+import Data.ByteString
 import Data.Constraint.Extras
 import Data.Constraint.Forall
 import Data.Functor.Identity
 import Data.Maybe
 import Data.Text
+import Data.Text.Encoding (encodeUtf8)
 import qualified Data.Text.Encoding as T
 import Data.Time
 import Database.Beam
@@ -32,6 +38,7 @@ import Database.Beam.Schema
 import Database.PostgreSQL.Simple.Beam
 import Rhyolite.Account
 import Rhyolite.DB.NotifyListen
+import Rhyolite.DB.NotifyListen.Beam
 
 -- | Postgres @current_timestamp()@ function. Returns the server's timestamp
 current_timestamp_ :: QExpr Postgres s UTCTime
@@ -77,3 +84,67 @@ login accountTable email pass = runMaybeT $ do
   pwHash <- MaybeT $ pure mPwHash
   guard $ verifyPasswordWith pbkdf2 (2^) (T.encodeUtf8 pass) pwHash
   pure (AccountId aid)
+
+ensureAccountExists
+  :: (Database Postgres db, HasNotification n Account, Has' ToJSON n Identity, ForallF ToJSON n)
+  => DatabaseEntity Postgres db (TableEntity Account)
+  -> Text
+  -> Pg (Bool, PrimaryKey Account Identity)
+ensureAccountExists accountTable email = do
+  existingAccountId <- runSelectReturningOne $ select $ fmap primaryKey $ filter_ (\x ->
+    lower_ (_account_email x) ==. lower_ (val_ email)) $ all_ accountTable
+  case existingAccountId of
+    Just existing -> return (False, existing)
+    Nothing -> do
+      results <- runInsertReturningList $ insert accountTable $ insertExpressions
+        [ Account
+            { _account_id = default_
+            , _account_email = lower_ (val_ email)
+            , _account_password = nothing_
+            , _account_passwordResetNonce = nothing_
+            }
+        ]
+      case results of
+        [acc] -> do
+          let aid = primaryKey acc
+          notify NotificationType_Insert (notification accountTable) aid
+          pure (True, aid)
+        _ -> error "ensureAccountExists: Creating account failed"
+
+setAccountPasswordHash
+  :: DatabaseEntity Postgres db (TableEntity Account)
+  -> PrimaryKey Account Identity
+  -> ByteString
+  -> Pg ()
+setAccountPasswordHash accountTable aid hash = runUpdate $ update accountTable
+  (\x -> mconcat
+    [ _account_password x <-. val_ (Just hash)
+    , _account_passwordResetNonce x <-. nothing_
+    ]
+  )
+  (\x -> primaryKey x ==. val_ aid)
+
+makePasswordHash
+  :: MonadIO m
+  => Text
+  -> m ByteString
+makePasswordHash pw = do
+  salt <- liftIO genSaltIO
+  return $ makePasswordSaltWith pbkdf2 (2^) (encodeUtf8 pw) salt 14
+
+resetPassword
+  :: (Database Postgres db)
+  => DatabaseEntity Postgres db (TableEntity Account)
+  -> PrimaryKey Account Identity
+  -> UTCTime
+  -> ByteString
+  -> Pg (Maybe (PrimaryKey Account Identity))
+resetPassword accountTable aid nonce pwhash = do
+  macc <- runSelectReturningOne $ lookup_ accountTable aid
+  case macc of
+    Nothing -> return Nothing
+    Just a -> if _account_passwordResetNonce a == Just nonce
+      then do
+        setAccountPasswordHash accountTable aid pwhash
+        return $ Just aid
+      else fail "nonce mismatch"
