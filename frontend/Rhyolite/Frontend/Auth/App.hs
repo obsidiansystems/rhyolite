0413a04e5da8fce4d9fc1800fa2abfe4c19cf822
Dan Bornside <dan.bornside@gmail.com>
2022-02-28 20:05:13 -0500

    actually use type aliases.

--- frontend/Rhyolite/Frontend/Auth/App.hs
+++ frontend/Rhyolite/Frontend/Auth/App.hs
@@ -7,16 +7,33 @@ import qualified Rhyolite.Frontend.Auth as Base
 import Rhyolite.Vessel.App as X
 import Control.Monad.Fix
 import Reflex
-import Data.Functor.Const
-import Data.Functor.Identity
 
+
+-- | The type for app widgets at the top level of the app.
+-- Includes full Auth handling for queries and requests.
+type FullAppWidget t app m =
+  Base.FullWidget t (AuthCredential app) (PublicApi app) (PrivateApi app) (PublicV app) (PrivateV app) (PersonalV app) m
+
+-- | The type for app widgets that have been specialized to a particular
+-- authenticated identity that may or may not be valid, and so the queries
+-- can fail.
+type AuthErrorAppWidget t app m =
+  Base.AuthErrorWidget t (AuthCredential app) (PublicApi app) (PrivateApi app) (PublicV app) (PrivateV app) (PersonalV app) m
+
+-- | The type for app widgets that have been specialized to a particular
+-- authenticated identity where authentication errors have already been handled
+-- so the queries cannot fail within this widget.
+type AuthAppWidget t app m =
+  Base.AuthWidget t (AuthCredential app) (PublicApi app) (PrivateApi app) (PublicV app) (PrivateV app) (PersonalV app) m
 
 -- | Embeds a widget that uses a specific auth identity into a context where no auth identity is presumed.
 authenticatedWidget
   :: ( MonadFix m, PostBuild t m
      , RhyoliteAuthApp app )
   => proxy app
-  -> (AuthCredential app)
-  -> QueryT t (FullAppAuthErrorV app (Const SelectedCount)) (RequesterT t (FullAuthApi app) Identity m) a
-  -> QueryT t (FullAppV app (Const SelectedCount)) (RequesterT t (FullApi app) Identity m) a
+  -> AuthCredential app
+  -> AuthErrorAppWidget t app m a
+  -> FullAppWidget t app m a
 authenticatedWidget _ = Base.authenticatedWidget
+
+
