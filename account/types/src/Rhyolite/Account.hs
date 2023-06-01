5fa1df57de0c7a8de75f4a4e64f290724f4c92cd
Cale Gibbard <cgibbard@gmail.com>
2023-06-01 14:17:44 -0400

    Fix spelling HasAccountConstrant -> HasAccountConstraint

--- account/types/src/Rhyolite/Account.hs
+++ account/types/src/Rhyolite/Account.hs
@@ -30,15 +30,15 @@ data Account f = Account
   , _account_passwordResetNonce :: Columnar f (Maybe UTCTime)
   } deriving (Generic)
 
-type HasAccountConstrant (c :: * -> Constraint) f =
+type HasAccountConstraint (c :: * -> Constraint) f =
   ( c (Columnar f (SqlSerial Int64))
   , c (Columnar f Text)
   , c (Columnar f (Maybe ByteString))
   , c (Columnar f (Maybe UTCTime))
   )
 
-deriving instance HasAccountConstrant Eq f => Eq (Account f)
-deriving instance HasAccountConstrant Ord f => Ord (Account f)
+deriving instance HasAccountConstraint Eq f => Eq (Account f)
+deriving instance HasAccountConstraint Ord f => Ord (Account f)
 
 instance Beamable Account
 
@@ -51,16 +51,16 @@ instance Table Account where
 
 instance Beamable (PrimaryKey Account)
 
-type HasAccountIdConstrant (c :: * -> Constraint) f = c (Columnar f (SqlSerial Int64))
+type HasAccountIdConstraint (c :: * -> Constraint) f = c (Columnar f (SqlSerial Int64))
 
-deriving instance HasAccountIdConstrant ToJSON f => ToJSON (PrimaryKey Account f)
-deriving instance HasAccountIdConstrant FromJSON f => FromJSON (PrimaryKey Account f)
-deriving instance HasAccountIdConstrant ToJSONKey f => ToJSONKey (PrimaryKey Account f)
-deriving instance HasAccountIdConstrant FromJSONKey f => FromJSONKey (PrimaryKey Account f)
+deriving instance HasAccountIdConstraint ToJSON f => ToJSON (PrimaryKey Account f)
+deriving instance HasAccountIdConstraint FromJSON f => FromJSON (PrimaryKey Account f)
+deriving instance HasAccountIdConstraint ToJSONKey f => ToJSONKey (PrimaryKey Account f)
+deriving instance HasAccountIdConstraint FromJSONKey f => FromJSONKey (PrimaryKey Account f)
 
-deriving instance HasAccountIdConstrant Eq f => Eq (PrimaryKey Account f)
-deriving instance HasAccountIdConstrant Ord f => Ord (PrimaryKey Account f)
-deriving instance HasAccountIdConstrant Show f => Show (PrimaryKey Account f)
+deriving instance HasAccountIdConstraint Eq f => Eq (PrimaryKey Account f)
+deriving instance HasAccountIdConstraint Ord f => Ord (PrimaryKey Account f)
+deriving instance HasAccountIdConstraint Show f => Show (PrimaryKey Account f)
 
 newtype PasswordResetToken = PasswordResetToken
   { unPasswordResetToken :: (PrimaryKey Account Identity, UTCTime)
